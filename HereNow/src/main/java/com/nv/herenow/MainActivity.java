e530747e92a34be77c4c1e7597e9cc9db60c7594
Vasudeva Nagaraja <nvasudeva88@gmail.com>
2013-07-26 22:14:22 -0700

    Scroll view listing playlists with radio buttons

--- HereNow/src/main/java/com/nv/herenow/MainActivity.java
+++ HereNow/src/main/java/com/nv/herenow/MainActivity.java
@@ -1,13 +1,23 @@
 package com.nv.herenow;
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -16,7 +26,26 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.activity_main, null);
+
+        // Find the ScrollView
+        if (v != null) {
+            ScrollView sv = (ScrollView) v.findViewById(R.id.scrollView);
+        }
+
+        // Create a LinearLayout element
+        LinearLayout ll = null;
+        if (v != null) {
+            ll = (LinearLayout)v.findViewById(R.id.linear_scrollView);
+        }
+
+//        List<RadioButton> buttonList = new ArrayList<RadioButton>();
+        RadioGroup rg = new RadioGroup(this); //create the RadioGroup
+        rg.setOrientation(RadioGroup.VERTICAL);//or RadioGroup.VERTICAL
+
+        //setContentView(R.layout.activity_main);
 
         final ContentResolver resolver = this.getContentResolver();
         final Uri uri = MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI;
@@ -33,7 +62,15 @@ public class MainActivity extends Activity {
         for (boolean hasItem = playLists.moveToFirst(); hasItem; hasItem = playLists.moveToNext()) {
             playListName = playLists.getString(playLists.getColumnIndex(nameKey));
             Log.i(LOGGING_TAG, playListName);
+            RadioButton newButton = new RadioButton(this);
+            rg.addView(newButton); //the RadioButtons are added to the radioGroup instead of the layout
+            newButton.setText(playListName);
         }
+
+        if (ll != null) {
+            ll.addView(rg);//you add the whole RadioGroup to the layout
+        }
+        setContentView(v);
     }
 
 
